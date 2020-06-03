11be822f4bd1a195bc80968090724822184c3b8c
Owoeye1321 <64838533+Owoeye1321@users.noreply.github.com>
2020-06-03 09:16:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 </head>
 <body>
 <?php
-            function greet_hng($name,$hng_id,$language){
+            function greet_hng($name,$hng_id,$email,$language){
                 
-                $output = "Hello world,this is ".$name." with HNGi7 ID: ".$hng_id." using language "
+                $output = "Hello world,this is ".$name." with HNGi7 ID: ".$hng_id."Email: ".$email." using language "
                 .$language." for stage 2 TEST RESULT:<i style ='color:green;'>Pass</i> ";
                 return $output;
             }
              
-             echo  greet_hng("OWOEYE SAMUEL","HNG-02249","PHP");
+             echo  greet_hng("OWOEYE SAMUEL","HNG-02249","Owoeye1321@gmail.com","PHP");
 
 
 ?>
